2a5f8db60c6700ed8c3e7231dd33f1b867161fd1
Jeric Bas <jericrayebas@gmail.com>
2022-07-25 23:28:19 +0800

    ssg next js

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte'
-import './index.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-
-const app = new App({
-    target: document.getElementById('app')
-  })
-
-export default app
